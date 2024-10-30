01fa2bf2723fca2eccfba7e6bbc36b3005f47a34
TaekyungHa <86tkstar@gmail.com>
2024-10-30 12:25:25 +0900

    App Setup and Expo Prebuild

---
+++ app/index.tsx
@@ -0,0 +1,17 @@
+import { Text, View } from "react-native";
+
+function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Hello Threads!</Text>
+    </View>
+  );
+}
+
+export default Index;
